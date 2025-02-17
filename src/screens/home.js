1b2ee35d92c9818492821a7ea5a54859b64e4321
GuiQRSM <quaresmagui20@gmail.com>
2025-02-17 11:33:14 -0300

    add a button

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,22 +1,38 @@
-import { Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function Home() {
+export default function Profile() {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#000',
-      }}
-    >
-      <Text
+    <View style={styles.container}>
+      <Text style={styles.text}>Tela de início</Text>
+      <TouchableOpacity
         style={{
-          color: '#fff',
+          borderRadius: 1,
+          borderColor: '#999',
+          marginVertical: 8,
+          padding: 4,
+          borderWidth: 1,
         }}
+        activeOpacity={0.6}
       >
-        Tela de início
-      </Text>
+        <Button></Button>
+        <Text style={styles.textButton}>Salvar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000',
+  },
+  text: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  textButton: {
+    color: '#fff',
+  },
+});
